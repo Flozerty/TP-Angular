5ec9e674ccc8b7158774873e2a9f651371fa139a
Floris Louërat <floris.louerat@gmail.com>
2023-11-23 11:11:03 +0100

    2 components

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlanetsComponent,
+    SideBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
