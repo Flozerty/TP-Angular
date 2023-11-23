cb8c524ba70dc378a4d40349c397f8ebac49d747
Floris Louërat <floris.louerat@gmail.com>
2023-11-23 11:43:28 +0100

    api

--- src/app/planets/planets.component.ts
+++ src/app/planets/planets.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-planets',
   templateUrl: './planets.component.html',
   styleUrl: './planets.component.scss'
 })
-export class PlanetsComponent {
+export class PlanetsComponent implements OnInit {
 
+  planets: any = { bodies: [] };
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+
+    this.http.get<any>('https://api.le-systeme-solaire.net/rest/bodies/').subscribe((data: any) => {
+      this.planets.bodies = data.bodies;
+      console.log(this.planets);
+    });
+  }
 }
